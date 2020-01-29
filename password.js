4629b6f1929bb8ae7a65f9439a22d982876a033a
pcchiu96 <pcchiu96@gmail.com>
2020-01-29 12:55:13 -0800

    Added css styling. Added speed info to screen. Optimized speed.

--- password.js
+++ password.js
@@ -5,13 +5,26 @@ document.getElementById("unlock").addEventListener("click", unlockPassword);
 
 function unlockPassword(){
     let password = document.getElementById("password").value;
-    unlock(password);
+    let print = document.getElementById("print");
+    if (password){
+        let t0 = performance.now();
+        let unlocked = unlock(password);
+        let t1 = performance.now();
+        console.log('Took', (t1 - t0).toFixed(4), 'milliseconds');
+
+        print.innerHTML = "Password: " + unlocked.password + "<br>"
+        + "Amount of tries: " + unlocked.tries + "<br>"
+        + "Time it took: " + (t1 - t0).toFixed(4) + " ms";
+    } else {
+        console.log("No input");
+    }   
 }
 
 //this generates the password index with each key as digit {'a' : 11} so it can be used to find the index
 function generatePasswordIndex(passwordSet){
     let obj = {};
-    for (let i = 0; i < passwordSet.length; i++) {
+    let length = passwordSet.length;
+    for (let i = 0; i < length; i++) {
         obj[passwordSet[i]] = i;
     }
     return obj;
@@ -23,6 +36,7 @@ function unlock(password){
 
     let lastDigit = passwordSet[passwordSet.length - 1];
     let firstDigit = passwordSet[0];
+    let passwordLength = passwordSet.length;
     let index = 0;
     let increment = 0;
     let pointer = 1;
@@ -30,8 +44,10 @@ function unlock(password){
     let next = '';
     let digitIndex = 0;
 
+    let process = document.getElementById("process");
+
     while (password !== runner){
-        if (increment === passwordSet.length) { //one tick
+        if (increment === passwordLength) { //one tick
             if (isFlip(runner, lastDigit)){ //at max digit value
                 runner = addDigit(passwordSet, runner); //add a digit
                 runner = resetAllBefore(runner, runner.length, firstDigit); //reset all values before it to default
@@ -43,7 +59,7 @@ function unlock(password){
                     next = runner.charAt(pointer);
                 }
 
-                digitIndex = passwordSetIndex[next] + 1;
+                digitIndex = passwordSetIndex[next] + 1; //
 
                 runner = replaceAt(runner, passwordSet[digitIndex], pointer);
                 runner = resetAllBefore(runner, pointer, firstDigit);
@@ -56,16 +72,22 @@ function unlock(password){
         runner = replaceAt(runner, passwordSet[increment], index);
         increment++;
 
-        console.log(runner);
-        n++;
+        //console.log(runner);
+        n++; 
     }
     console.log("Took " + n + " many tries.");
+
+    return {
+        "password": runner,
+        "tries": n
+    }
 }
 
 //this function checks if the string consists of the last digit, returns true if it is and false if otherwise
 function isFlip(str, lastDigit){
     let array = [...str];
-    for (let i = 0; i < str.length; i++){
+    let length = str.length;
+    for (let i = 0; i < length; i++){
         if (array[i] !== lastDigit){
             return false;
         }
